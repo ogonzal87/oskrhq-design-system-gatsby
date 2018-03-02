5fa026e66f2ab53650cd705f354fc5de32c22982
ogonzal <ogonzalez2@w10vswamt44.corp.elmae>
2018-03-01 23:38:20 -0800

    fixing annoying aligment on the color picture

--- src/pages/color.js
+++ src/pages/color.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import DSLeftNav from '../components/LeftNav'
-import PrimaryColorsImg from '../assets/color-page/ds-primary-colors.png'
+import PrimaryColorsImg from '../assets/color-page/ds-primary-colorss.png'
 
 const Color = () => (
   <div className="ds-app-grid">
