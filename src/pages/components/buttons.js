ff56d534b43c1d8283f029078897ab1d8526287b
ogonzal <ogonzal87@gmail.com>
2018-03-16 23:38:45 -0700

    working on the buttons page

--- src/pages/components/buttons.js
+++ src/pages/components/buttons.js
@@ -2,10 +2,23 @@ import React from 'react'
 import Link from 'gatsby-link'
 import DSLeftNav from '../../components/ds-website-components/LeftNav'
 import Highlight from 'react-highlight'
-import '../../../node_modules/highlight.js/styles/tomorrow-night-eighties.css'
+import '../../../node_modules/highlight.js/styles/dracula.css'
 import FooterMessage from '../../components/ds-website-components/FooterMessage';
 import DSButton from '../../components/Button/index'
+import styled from 'styled-components';
+import '../../scss/_styles.scss';
 
+const SpecimenWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-column-gap: 16px;
+`
+const SpecimenSizeWrapper = styled.div`
+  margin-top: 80px;
+  display: grid;
+  grid-template-row: repeat(3, 1fr);
+  grid-row-gap: 16px;
+`
 
 const ButtonsPage = () => (
   <div className="ds-app-grid">
@@ -15,10 +28,38 @@ const ButtonsPage = () => (
       <div className="ds-container">
         <h1>Buttons</h1>
 
-        <h3 className="subheading">These are buttons</h3>
+        <h3 className="subheading">Buttons make common actions immediately visible and easy to perform with one click or tap. </h3>
 
-        <DSButton />
+        <SpecimenWrapper>
+          <DSButton label="Primary" type="primary"/>
+          <DSButton label="Secondary" type="secondary"/>
+          <DSButton label="success" type="success"/>
+          <DSButton label="info" type="info"/>
+          <DSButton label="danger" type="danger" />
+          <DSButton label="ghost" type="ghost" />
+        </SpecimenWrapper>
+
+        <SpecimenSizeWrapper>
+          <DSButton label="Small" type="primary" size="small" />
+          <DSButton label="Medium" type="primary" size="medium" />
+          <DSButton label="Fluid" type="primary" />
+        </SpecimenSizeWrapper>        
+        
+        <h4 className="ds-usage-title">Usage</h4>
+
+        <p>All UI elements make use of the sass function <code>ds-color($variable, $range)</code>. This ensures consistency since all colors are extracted from the same base. Avoid hard coding #HEX values when creating new UI elements or pages.</p>
+
+        <Highlight className='html'>
+          {`<DSButton 
+  label="save"
+  type="primary"
+  size="fluid"
+/>`}
+        </Highlight>
+
       </div>
+
+      <FooterMessage />
     </div>
   </div>
 )
