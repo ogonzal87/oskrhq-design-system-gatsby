b0e8d4407df9a926c3c0e91bee35d22b4be82ca7
ogonzal <ogonzal87@gmail.com>
2018-03-29 23:50:53 -0700

    better wording on the intro page

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = () => (
 
         <div className="ds-col-container">
           <div className="ds-col-5">
-            <p>OSKRHQ.DS is a Styleguide AND a Component Library written in React for both web and mobile. Although this website consumes the system, the site of is just a face for what it really is. The magic is in the code and how Design and Code are able to coexists in one sole language, allowing teams to compose many products from the same Brand Identity.</p>
+            <p>OSKRHQ.DS is a Styleguide AND a Component Library written in React for both web and mobile. Although this website consumes the system, the site of is just a face for what it really is. The magic is in the code which allows Design and Code to coexists in one sole visual language. All this, so that product teams can compose many products from the same Brand Identity or UI Anatomy.</p>
 
             <p>Every UI element you see here was composed from the same 5 foundational elements or what I call <a href="https://medium.muz.li/the-anatomy-of-all-digital-interfaces-11d43f55eaf" target="_blank">The Anatomy of UI</a>; Color, Typography, Space, Motion, and Depth</p>
           </div>
@@ -49,11 +49,11 @@ const IndexPage = () => (
 
         <div className="ds-col-container">
           <div className="ds-col-5">
-            <p>Everything you see was designed at some point. However, as Brian Reed would say, few things are designed well. In most companies, interfaces are crafted in silos by smaller teams. This isolation creates fragmented experiences in the form of inconsistencies looking at the product holistically. Coupled with headaches when trying to communicate designs to developers, it can get really ugly for the user since their experience is the interaction with the whole product, not just a specific interface.</p>
+            <p>Every interface you interact with was designed at some point. And those interfaces were probably crafted by design teams divided into smaller teams. These silos create fragmented experiences looking at the product holistically. Coupled with headaches when trying to communicate designs to developers, it can get really ugly for the user since their experience is the interaction with the whole product, not just a specific part of it.</p>
           </div>
 
           <div className="ds-col-7">
-            <p className="ds-headline-text-style">The System is a <span className="ds-accent-text">Visual Language</span>. A language that allows designers and developers to communicate with one another, amongst each other, and it allows your <span className="ds-accent-text">brand</span> to communicate with your users through carefully crafted and hopefully cohesive experiences.</p>
+            <p className="ds-headline-text-style">Design Systems are a <span className="ds-accent-text">Visual Language</span>. A language that allows designers and developers to communicate with one another, amongst each other, and it allows your <span className="ds-accent-text">brand</span> to communicate with your users through carefully crafted and hopefully cohesive experiences.</p>
           </div>
         </div>
         
@@ -66,9 +66,11 @@ const IndexPage = () => (
         <p className="ds-button-text-style ds-intro-subtitle">How Do I use it?</p>
 
         <div className="ds-col-container">
-          <div className="ds-col-12">
+          <div className="ds-col-6">
             <p>If you are a Designer... I got you. I created a <a href="https://github.com/ogonzal87/oskrhq-uikit" target="blank">UI Kit</a> that mirrors the components you see in this website. That way, you can create experiences with the same building blocks (components) the Developers have. It also has all the design assets you will need to get started.</p>
+          </div>
 
+          <div className="ds-col-6">
             <p>If you are a Developer, check the <a href="https://github.com/ogonzal87/oskrhq-design-system-react" target="blank">Github repo</a> from guidance. And if you feel like contributing, it would make my day 😎</p>
           </div>
         </div>
