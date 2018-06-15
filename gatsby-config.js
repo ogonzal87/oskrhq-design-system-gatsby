56395c004801d858148133e1539f799a9acafed7
ogonzal87 <ogonzal87@gmail.com>
2018-06-14 19:50:46 -0700

    adding google analytincs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  pathPrefix: `/oskrhq-design-system-website`,
+  pathPrefix: '/oskrhq-design-system-website',
   siteMetadata: {
-    title: 'OSKRHQ Design System',
+    title: "OSKRHQ Design System"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
-      resolve: `gatsby-plugin-favicon`,
+      resolve: 'gatsby-plugin-favicon',
       options: {
         logo: "./src/favicon.png",
         injectHTML: true,
@@ -22,6 +22,20 @@ module.exports = {
           yandex: false,
           windows: false
         }
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: "UA-120926305-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"]
       }
     }
   ]
