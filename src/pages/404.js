82a418a213d0faa9cf26047a59f7cadb30e95388
ogonzal87 <ogonzal87@gmail.com>
2018-09-11 00:33:09 -0700

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,50 @@
 import React from 'react'
 import { DSButton } from 'oskrhq-design-system'
 import Link from "gatsby-link";
+import styled from 'styled-components'
 
-const goBack = () => {
-  console.log(location)
-}
+const Text = styled.div`
+  z-index: 1;
+  h1, h5 {
+    background: #5252ed;
+    color: white;
+  }
+
+  h5 {
+    margin-bottom: 40px;
+  }
+`
 
 const NotFoundPage = () => (
-  <div className="ds-main-content" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexFlow: 'column' }}>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness 😞</p>
+  <div className="ds-main-content" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexFlow: 'column', height: '100vh' }}>
 
-    <Link to="/">
-      <DSButton
-        type="primary"
-        size=""
-        onClick={goBack}>
-        Go Home
-      </DSButton>
-    </Link>
+    <Text>
+      <h1>SIKEEE 404!</h1>
+      <h5>You arrived to a place that doesn&#39;t exist...</h5>
+      <Link to="/" >
+        <DSButton style={{ float: 'right', marginTop: '40px' }}
+          type="primary"
+          size="medium">
+          Go Home
+        </DSButton>
+      </Link>
+    </Text>
+
+
+    <div
+      style={{ width: '100%', height: '100%', position: 'absolute' }}>
+      <iframe
+        src="https://giphy.com/embed/1GIiIIDxPdzkQ"
+        width={'100%'}
+        height={'100%'}
+        style={{ position: "absolute" }}
+        frameBorder="0"
+        className="giphy-embed"
+        allowFullScreen></iframe>
+    </div>
+
+
+
   </div>
 )
 
